ee7b26b0a11e5614365fbf53a4876e02da190090
IndraP24 <indrashis985@gmail.com>
2021-08-14 13:03:05 +0530

    Add SwitchCases.java

---
+++ src/Fundamentals/SwitchCases.java
@@ -0,0 +1,9 @@
+package Fundamentals;
+
+import java.util.Scanner;
+
+public class SwitchCases {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
+}
