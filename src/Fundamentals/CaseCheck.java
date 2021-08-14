37555c0e571e5718e4a5786c7a3419c36531dde2
IndraP24 <indrashis985@gmail.com>
2021-08-14 13:02:03 +0530

    Add CaseCheck.java

---
+++ src/Fundamentals/CaseCheck.java
@@ -0,0 +1,16 @@
+package Fundamentals;
+
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char ch = in.next().trim().charAt(0);
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("LowerCase");
+        } else {
+            System.out.println("UpperCase");
+        }
+        System.out.println(ch);
+    }
+}
