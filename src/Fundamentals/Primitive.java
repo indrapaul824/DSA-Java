86d090e2b3f8d53d5aea3e519cd5915f185b7822
IndraP24 <indrashis985@gmail.com>
2021-08-14 13:04:10 +0530

    Add Primitive.java

---
+++ src/Fundamentals/Primitive.java
@@ -0,0 +1,15 @@
+package Fundamentals;
+
+public class Primitive {
+    public static void main(String[] args) {
+        // Data types
+        int rollno = 32;
+        char letter = 'r';
+        float marls = 98.67f;
+        double largeD = 762598179.087086;
+        long largeI = 80976271805906597L;
+        boolean check = false;
+
+
+    }
+}
