3b65919c38f110b5cae40f26950194dd019156b3
IndraP24 <indrashis985@gmail.com>
2021-09-11 23:41:38 +0530

    Update with commented Solution

--- TypeScript/Big O/Ex2.ts
+++ TypeScript/Big O/Ex2.ts
@@ -1,18 +1,22 @@
 function anotherFunChallenge(input: Array<string>) {
-    let a = 5;
-    let b = 10;
-    let c = 50;
+    let a = 5; // O(1)
+    let b = 10; // O(1)
+    let c = 50; // O(1)
 
     for (let i = 0; i < input.length; i++) {
-        let x = i + 1;
-        let y = i + 2;
-        let z = i + 3;
+        // O(n)
+        let x = i + 1; // O(n)
+        let y = i + 2; // O(n)
+        let z = i + 3; // O(n)
     }
 
     for (let j = 0; j < input.length; j++) {
-        let p = j * 2;
-        let y = j * 2;
+        // O(n)
+        let p = j * 2; // O(n)
+        let y = j * 2; // O(n)
     }
 
-    let whoAmI = "I don't know!";
+    let whoAmI = "I don't know!"; // O(1)
 }
+
+// Total Complexity: 4*1 + 7*O(n) == O(4 + 7n) === O(n) ==> Linear Time
