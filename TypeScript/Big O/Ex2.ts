88fac6b86988872b06c06235db9b0cf6eba24ef9
IndraP24 <indrashis985@gmail.com>
2021-09-11 23:39:33 +0530

    Add Ex2.ts

---
+++ TypeScript/Big O/Ex2.ts
@@ -0,0 +1,18 @@
+function anotherFunChallenge(input: Array<string>) {
+    let a = 5;
+    let b = 10;
+    let c = 50;
+
+    for (let i = 0; i < input.length; i++) {
+        let x = i + 1;
+        let y = i + 2;
+        let z = i + 3;
+    }
+
+    for (let j = 0; j < input.length; j++) {
+        let p = j * 2;
+        let y = j * 2;
+    }
+
+    let whoAmI = "I don't know!";
+}
