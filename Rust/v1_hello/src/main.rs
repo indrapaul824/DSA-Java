6f6f6460d9b29b2ada1a16f528a0e1f24ced21b1
IndraP24 <indrashis985@gmail.com>
2021-09-22 21:50:06 +0530

    Add usage of basic rust struct

--- Rust/v1_hello/src/main.rs
+++ Rust/v1_hello/src/main.rs
@@ -1,3 +1,21 @@
+pub struct Person {
+    name: String,
+    age: i32,
+    children: i32,
+}
+
+impl Person {
+    pub fn print(self) -> String {
+        format!("name = {}, age = {}, has {} children", self.name, self.age, self.children)
+
+    }
+}
+
 fn main() {
-    println!("Hello, algorithms people!");
+    let p = Person{
+        name: "matt".to_string(),
+        age: 35,
+        children: 4
+    };
+    println!("Hello, people, from {}", p.print());
 }
