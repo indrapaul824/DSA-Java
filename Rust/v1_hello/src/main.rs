64662a328c60a75878ed9b34d16dcfc7f6eb7c20
IndraP24 <indrashis985@gmail.com>
2021-09-22 22:08:18 +0530

    Add enum functionality

--- Rust/v1_hello/src/main.rs
+++ Rust/v1_hello/src/main.rs
@@ -1,9 +1,16 @@
 #[derive(Debug)]
-
 pub struct Person {
     name: String,
     age: i32,
     children: i32,
+    fave_color: Color,
+}
+
+#[derive(Debug)]
+pub enum Color {
+    Red(String),
+    Green,
+    Blue,
 }
 
 impl Person {
@@ -17,7 +24,15 @@ fn main() {
     let p = Person{
         name: "matt".to_string(),
         age: 35,
-        children: 4
+        children: 4,
+        fave_color: Color::Green,
     };
     println!("Hello, people, from {:?}", p);
+
+    let c = Color::Red("hello".to_string());
+    match c {
+        Color::Red(s) => println!("It's Red {}", s),
+        Color::Green => println!("It's Green"),
+        Color::Blue => println!("It's Blue"),
+    }
 }
