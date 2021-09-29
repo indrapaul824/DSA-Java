a673f0a1beb96fbb26e6a6d915a2055b2c25993f
IndraP24 <indrashis985@gmail.com>
2021-09-29 21:50:49 +0530

    Rename file

--- TypeScript/Big O/BigO_1.ts
+++ TypeScript/Big O/BigO_1.ts
@@ -24,18 +24,3 @@ function findNemo(array: Array<String>) {
 }
 
 findNemo(large); // O(n) ---> Linear Time
-
-var boxes: Array<string | number> = [0, 1, 2, 3, 4, 5, 6];
-
-function compressFirstBox(boxes: Array<String | number>) {
-    console.log(boxes[0]);
-}
-
-compressFirstBox(large); // Time Complexity: O(1)
-
-function logFirstTwoBoxes(boxes: Array<String | number>) {
-    console.log(boxes[0]);
-    console.log(boxes[1]);
-}
-
-logFirstTwoBoxes(boxes); // Time Complexity: O(2)
