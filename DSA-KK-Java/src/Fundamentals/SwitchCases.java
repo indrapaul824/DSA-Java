ab49ecbe2a74cad1fbcc903a1f6fe774653547b2
IndraP24 <indrashis985@gmail.com>
2022-01-25 00:10:33 +0530

    Add enhanced switch-case statement example

--- DSA-KK-Java/src/Fundamentals/SwitchCases.java
+++ DSA-KK-Java/src/Fundamentals/SwitchCases.java
@@ -41,5 +41,41 @@ public class SwitchCases {
             case "Grapes" -> System.out.println("Small fruit");
             default -> System.out.println("Please enter a valid fruit!");
         }
+
+        int day = in.nextInt();
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+        }
+
+
+        // Another example where it will print Weekday for each 1 to 5 and Weekend for 6 and 7
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Weekday");
+                break;
+            case 6:
+            case 7:
+                System.out.println("Weekend");
+                break;
+        }
+
+        // Same with enhanced switch-case
+        switch (day) {
+            case 1, 2, 3, 4, 5 -> System.out.println("Weekday");
+            case 6, 7 -> System.out.println("Weekend");
+        }
+
+
+
     }
 }
