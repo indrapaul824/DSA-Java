e362c61e4c684664eebcc56b12173900b189a383
IndraP24 <indrashis985@gmail.com>
2022-01-24 23:52:42 +0530

    Add enhanced switch-case statement example

--- DSA-KK-Java/src/Fundamentals/SwitchCases.java
+++ DSA-KK-Java/src/Fundamentals/SwitchCases.java
@@ -8,15 +8,38 @@ public class SwitchCases {
         String fruit = in.next();
 
         // Using if-statements
-        if (fruit.equals("mango")) {
+        if (fruit.equals("Mango")) {
             System.out.println("King of fruits");
         }
 
-        if (fruit.equals("apple")) {
+        if (fruit.equals("Apple")) {
             System.out.println("A sweet red fruit");
         }
 
-        // Using switch-case statements
+        // Using switch-case with a break statement
+        switch (fruit) {
+            case "Mango":
+                System.out.println("King of fruits");
+                break;
+            case "Apple":
+                System.out.println("A sweet red fruit");
+                break;
+            case "Orange":
+                System.out.println("Round fruit");
+            case "Grapes":
+                System.out.println("Small fruit");
+                break;
+            default:
+                System.out.println("Please enter a valid fruit!");
+        }
 
+        // Using enhanced switch-case with all break statements
+        switch (fruit) {
+            case "Mango" -> System.out.println("King of fruits");
+            case "Apple" -> System.out.println("A sweet red fruit");
+            case "Orange" -> System.out.println("Round fruit");
+            case "Grapes" -> System.out.println("Small fruit");
+            default -> System.out.println("Please enter a valid fruit!");
+        }
     }
 }
