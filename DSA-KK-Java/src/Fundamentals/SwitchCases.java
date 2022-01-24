0b14196d484e2e0ff43248571198112bca2ce37a
IndraP24 <indrashis985@gmail.com>
2022-01-24 23:46:14 +0530

    Update SwitchCases.java

--- DSA-KK-Java/src/Fundamentals/SwitchCases.java
+++ DSA-KK-Java/src/Fundamentals/SwitchCases.java
@@ -5,5 +5,18 @@ import java.util.Scanner;
 public class SwitchCases {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String fruit = in.next();
+
+        // Using if-statements
+        if (fruit.equals("mango")) {
+            System.out.println("King of fruits");
+        }
+
+        if (fruit.equals("apple")) {
+            System.out.println("A sweet red fruit");
+        }
+
+        // Using switch-case statements
+
     }
 }
