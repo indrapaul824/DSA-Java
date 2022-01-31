b146fe44691ddebb848b2913f8b5b0a07ec051c7
IndraP24 <indrashis985@gmail.com>
2022-01-31 23:50:34 +0530

    Add shadowing example

--- DSA-KK-Java/src/Fundamentals/Methods/Shadowing.java
+++ DSA-KK-Java/src/Fundamentals/Methods/Shadowing.java
@@ -1,9 +1,20 @@
 package Fundamentals.Methods;
 
 public class Shadowing {
-    static int x;
+    static int x = 90; // Class variable
+
     public static void main(String[] args) {
         // Shadowing is allowed in static methods
-        System.out.println(x);
+        System.out.println(x); // 90
+
+        int x; // Local variable
+        x = 100; // Local variable shadows the class variable only on initialization
+        System.out.println(x); // 100
+
+        printX();
+    }
+
+    static void printX() {
+        System.out.println(x); // 90
     }
 }
