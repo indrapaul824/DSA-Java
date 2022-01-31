347bb27cd9ae67fe4b418c52400eeaa1e43fb0ee
IndraP24 <indrashis985@gmail.com>
2022-01-31 23:50:24 +0530

    Update resource with try block

--- DSA-KK-Java/src/Fundamentals/Methods/Main.java
+++ DSA-KK-Java/src/Fundamentals/Methods/Main.java
@@ -4,15 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Q: Take input of 2 numbs and print the sum
-        Scanner in = new Scanner(System.in);
-        int a, b, sum;
+        try (
+                // Q: Take input of 2 numbs and print the sum
+                Scanner in = new Scanner(System.in)) {
+            int a, b, sum;
 
-        System.out.println("Enter two numbers");
-        a = in.nextInt();
-        b = in.nextInt();
+            System.out.println("Enter two numbers");
+            a = in.nextInt();
+            b = in.nextInt();
 
-        sum = a + b;
-        System.out.println("Sum is " + sum);
+            sum = a + b;
+
+            System.out.println("Sum is " + sum);
+        }
     }
 }
