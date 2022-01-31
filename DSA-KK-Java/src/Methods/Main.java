4e0e59bcaa99de715cd82210c0bc2e56620e81d6
IndraP24 <indrashis985@gmail.com>
2022-01-31 23:31:58 +0530

    Add Main class for Methods

--- DSA-KK-Java/src/Methods/Main.java
+++ DSA-KK-Java/src/Methods/Main.java
@@ -1,7 +1,18 @@
 package Methods;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // Q: Take input of 2 numbs and print the sum
+        Scanner in = new Scanner(System.in);
+        int a, b, sum;
 
+        System.out.println("Enter two numbers");
+        a = in.nextInt();
+        b = in.nextInt();
+
+        sum = a + b;
+        System.out.println("Sum is " + sum);
     }
 }
