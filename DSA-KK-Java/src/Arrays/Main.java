0f649cfaa8099579d14763897d22c44b9dc30faa
IndraP24 <indrashis985@gmail.com>
2022-02-01 23:35:50 +0530

    Add class for Arrays

---
+++ DSA-KK-Java/src/Arrays/Main.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // syntax: data_type[] var_name = new data_type[size];
+        int[] rnos = new int[5];
+        // OR
+        int[] rnos2 = {12, 23, 48, 89, 64};
+    }
+}
