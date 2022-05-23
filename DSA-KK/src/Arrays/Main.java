fa9cd0f39af6738d571fe6c8452a69daa1d07a5c
IndraP24 <indrashis985@gmail.com>
2022-05-23 15:20:04 +0530

    Update Main.java for Arrays

--- DSA-KK/src/Arrays/Main.java
+++ DSA-KK/src/Arrays/Main.java
@@ -6,5 +6,12 @@ public class Main {
         int[] rnos = new int[5];
         // OR
         int[] rnos2 = {12, 23, 48, 89, 64};
+        
+        System.out.println(rnos[1]); // Returns 0 -> Initialized to 0 since no value mentioned
+
+        System.out.println(rnos2[2]); // Returns 48 -> Initailized object in heap to value 48
+
+        String[] arr = new String[4];
+        System.out.println(arr[2]);
     }
 }
