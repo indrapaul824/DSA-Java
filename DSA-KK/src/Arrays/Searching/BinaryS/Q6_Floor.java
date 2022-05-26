4c17d6700449971e3da35cf3e4a324259fedab46
IndraP24 <indrashis985@gmail.com>
2022-05-27 01:01:17 +0530

    Update Floor and Ceil of an element

---
+++ DSA-KK/src/Arrays/Searching/BinaryS/Q6_Floor.java
@@ -0,0 +1,34 @@
+package Arrays.Searching.BinaryS;
+
+public class Q6_Floor {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 8, 10, 10, 19};
+        int target = 5;
+
+        System.out.println("The ceil of the target is: " + floor(arr, target));
+    }
+
+    public static int floor(int[] arr, int target) {
+
+        // Edge Case: If target < the smallest element in array, no floor
+        if (target < arr[arr.length - 1]) {
+            return -1;
+        }
+
+        int start = 0, end = arr.length - 1, floor = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if (arr[mid] == target)
+                return arr[mid];
+            else if (arr[mid] < target) {
+                floor = arr[mid];
+                start = mid + 1;
+            }
+            else
+                end = mid - 1;
+        }
+
+        return floor;
+    }
+}
---
+++ DSA-KK/src/Arrays/Searching/BinaryS/Q7_Ceil.java
@@ -0,0 +1,35 @@
+package Arrays.Searching.BinaryS;
+
+public class Q7_Ceil {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 8, 10, 10, 19};
+        int target = 5;
+
+        System.out.println("The ceil of the target is: " + ceil(arr, target));
+    }
+
+    public static int ceil(int[] arr, int target) {
+
+        // Edge Case: If target > the largest element in array, no ceil
+        if (target > arr[arr.length - 1]) {
+            return -1;
+        }
+
+        int start = 0, end = arr.length - 1, ceil = 0;
+
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+
+            if (arr[mid] == target)
+                return arr[mid];
+            else if (arr[mid] < target)
+                start = mid + 1;
+            else {
+                ceil = arr[mid];
+                end = mid - 1;
+            }
+        }
+
+        return ceil;
+    }
+}
