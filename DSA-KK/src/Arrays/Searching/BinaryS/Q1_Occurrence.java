4e224b294ae5971562de620f60246c3d1cba0714
IndraP24 <indrashis985@gmail.com>
2022-05-27 18:26:08 +0530

    Update Occurrence with a combined method for both

--- DSA-KK/src/Arrays/Searching/BinaryS/Q1_Occurrence.java
+++ DSA-KK/src/Arrays/Searching/BinaryS/Q1_Occurrence.java
@@ -1,11 +1,17 @@
 package Arrays.Searching.BinaryS;
 
+// Q 1 : Find the First and Last occurrence of an element in a Sorted Array
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+
+import java.util.Arrays;
+
 public class Q1_Occurrence {
     public static void main(String[] args) {
         int[] arr = {2, 4, 10, 10, 10, 18, 20};
         int target = 10;
         System.out.println("The 1st occurrence is at: " + occurFirst(arr, target));
         System.out.println("The Last occurrence is at: " + occurLast(arr, target));
+        System.out.println("The 1st and last occurences are at: " + Arrays.toString(searchRange(arr, target)));
     }
 
     public static int occurFirst(int[] arr, int target) {
@@ -46,5 +52,36 @@ public class Q1_Occurrence {
         }
 
         return  last;
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int[] range = {-1, -1};
+        range[0] = search(nums, target, true);
+
+        if (range[0] != -1)
+            range[1] = search(nums, target, false);
+
+        return range;
+    }
+
+    public static int search(int[] nums, int target, boolean firstIndex) {
+        int start = 0, end = nums.length - 1, range = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > target)
+                end = mid - 1;
+            else if (nums[mid] < target)
+                start = mid + 1;
+            else {    // if nums[mid] == target
+                range = mid;
+                if (firstIndex)
+                    end = mid -1;
+                else
+                    start = mid + 1;
+            }
+        }
+        return range;
     }
 }
