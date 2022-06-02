1efdb335319ff1ecbd3b54b235aefcb334c6430e
IndraP24 <indrashis985@gmail.com>
2022-06-02 23:51:48 +0530

    Add BS method for counting frequency of an element

--- DSA-KK/src/Arrays/Searching/BinaryS/Q2_Frequency.java
+++ DSA-KK/src/Arrays/Searching/BinaryS/Q2_Frequency.java
@@ -1,4 +1,45 @@
 package Arrays.Searching.BinaryS;
 
 public class Q2_Frequency {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 11;
+
+        System.out.println(findFrequency(arr, target));
+    }
+
+    static int findFrequency(int[] arr, int target){
+        int first = findOccurrence(arr, target, true), last;
+
+        if (first != -1) {
+            last = findOccurrence(arr, target, false);
+            return last - first + 1;
+        }
+
+        return 0;
+    }
+
+    static int findOccurrence(int[] arr, int target, boolean firstIndex) {
+        int start = 0, end = arr.length - 1, range = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] < target)
+                start = mid + 1;
+            else if (arr[mid] > target)
+                end = mid - 1;
+            else {
+                range = mid;
+
+                if (firstIndex)
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+        }
+
+        return range;
+    }
+
 }
