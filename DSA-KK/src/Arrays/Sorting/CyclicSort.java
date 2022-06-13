c4a1a93be5714e4a4fe6ba766a28b221b4575f8b
IndraP24 <indrashis985@gmail.com>
2022-06-13 17:14:30 +0530

    Add Cyclic Sort code implementation

---
+++ DSA-KK/src/Arrays/Sorting/CyclicSort.java
@@ -0,0 +1,24 @@
+package Arrays.Sorting;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 4, 3, 1};
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        cyclicSort(arr);
+        System.out.println("Sorted Array: " + Arrays.toString(arr));
+    }
+
+    public static void cyclicSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i=0; i < n; i++) {
+            if (arr[i] != i+1) {
+                int temp = arr[i];
+                arr[i] = arr[arr[i] - 1];
+                arr[arr[i] - 1] = temp;
+            }
+        }
+    }
+}
