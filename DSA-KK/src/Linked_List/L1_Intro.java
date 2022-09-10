1558bb833ce24aaaaff673104b1139350aa0bfef
IndraP24 <indrashis985@gmail.com>
2022-09-11 00:04:08 +0530

    Add Linked List class usage initial commit

---
+++ DSA-KK/src/Linked_List/L1_Intro.java
@@ -0,0 +1,5 @@
+package Linked_List;
+
+public class L1_Intro {
+    LL list = new LL();
+}
---
+++ DSA-KK/src/Linked_List/LL.java
@@ -0,0 +1,39 @@
+package Linked_List;
+
+public class LL {
+
+    private Node head;
+    private Node tail;
+
+    private int size;
+
+    public LL() {
+        this.size = 0;
+    }
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail == null)
+            tail = head;
+
+        size += 1;
+    }
+
+    private static class Node {
+        private int value;
+        private Node next;
+
+        public Node (int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+
+        }
+    }
+}
