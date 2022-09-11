affdeed2c58895e8e503a86ad7a459157ba34afe
IndraP24 <indrashis985@gmail.com>
2022-09-11 16:17:35 +0530

    Update implementation of insert and display func of LL class

--- DSA-KK/src/Linked_List/L1_Intro.java
+++ DSA-KK/src/Linked_List/L1_Intro.java
@@ -1,5 +1,15 @@
 package Linked_List;
 
 public class L1_Intro {
-    LL list = new LL();
+    public static void main(String[] args) {
+        LL list = new LL();
+
+        list.insertFirst(3);
+        list.insertFirst(5);
+        list.insertFirst(9);
+        list.insertFirst(8);
+        list.insertFirst(4);
+
+        list.display();
+    }
 }
