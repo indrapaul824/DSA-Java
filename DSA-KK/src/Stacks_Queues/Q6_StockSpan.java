a72056574b811cbc066dde799d542755f95b0212
IndraP24 <indrashis985@gmail.com>
2022-12-08 22:33:16 +0530

    Resolve start point bug in stack solution

--- DSA-KK/src/Stacks_Queues/Q6_StockSpan.java
+++ DSA-KK/src/Stacks_Queues/Q6_StockSpan.java
@@ -7,7 +7,7 @@ public class Q6_StockSpan {
         int n = arr.length;
         int[] output = new int[n];
         Deque<Map.Entry<Integer, Integer>> stack = new ArrayDeque<>();
-        stack.push(Map.entry(0, arr[0]));
+        stack.push(Map.entry(arr[0], 0));
 
         output[0] = 1;
 
@@ -15,16 +15,16 @@ public class Q6_StockSpan {
             while (!stack.isEmpty() && stack.peek().getKey() <= arr[i])
                 stack.pop();
             if (stack.isEmpty())
-                output[i] =  -1;
+                output[i] = i + 1;
             else
-                output[i] = stack.peek().getValue();
+                output[i] = i - stack.peek().getValue();
             stack.push(Map.entry(arr[i], i));
         }
         return output;
     }
 
     public static void main(String[] args) {
-        int[] arr = {100, 80, 60, 70, 60, 75, 85};
+        int[] arr = {90, 100, 80, 60, 70, 60, 75, 85};
         System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(stockSpan(arr)));
     }
