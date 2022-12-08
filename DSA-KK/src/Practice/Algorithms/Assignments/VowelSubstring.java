231ef56e25bab6513b4833eda156237fbc5ad208
IndraP24 <indrashis985@gmail.com>
2022-12-08 22:35:31 +0530

    Add Vowel Substring solution initial commit

---
+++ DSA-KK/src/Practice/Algorithms/Assignments/VowelSubstring.java
@@ -0,0 +1,15 @@
+package Practice.Algorithms.Assignments;
+
+import java.util.*;
+
+public class VowelSubstring {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String s = in.nextLine();
+
+        for (int i = 0; i < s.length(); i++) {
+
+        }
+    }
+}
