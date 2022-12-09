0fcecbcad9644aee285ba113d2aa9134f4c38bdb
IndraP24 <indrashis985@gmail.com>
2022-12-09 20:19:12 +0530

    Update solution using HashSet

--- DSA-KK/src/Practice/Algorithms/Assignments/VowelSubstring.java
+++ DSA-KK/src/Practice/Algorithms/Assignments/VowelSubstring.java
@@ -7,9 +7,22 @@ public class VowelSubstring {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the string:");
         String s = in.nextLine();
+        String vowel = "aeiou";
+        HashSet<Character> set = new HashSet<>();
+        int count = 0;
 
-        for (int i = 0; i < s.length(); i++) {
+        int j = 5, n = s.length();
 
+        for (int i = 0; i < n; i++) {
+            char t = s.charAt(i);
+            if (vowel.indexOf(t) != -1)
+                set.add(s.charAt(i));
+            else
+                set.clear();
+            if (set.size() == 5)
+                count++;
         }
+
+        System.out.println(count);
     }
 }
