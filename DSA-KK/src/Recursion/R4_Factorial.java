b0ab6e25dbe2e38738b2ef03ca58024535306351
IndraP24 <indrashis985@gmail.com>
2022-12-16 22:28:13 +0530

    Add Recursion Factorial of a num

--- DSA-KK/src/Recursion/R4_Factorial.java
+++ DSA-KK/src/Recursion/R4_Factorial.java
@@ -1,17 +1,13 @@
 package Recursion;
 
-public class R3_Factorial {
+public class R4_Factorial {
     public static void main(String[] args) {
         System.out.println(Factorial(3));
     }
-
     static int Factorial(int n) {
-
         if (n == 1) {
             return 1;
         }
-
         return Factorial(n-1) * n;
-
     }
 }
