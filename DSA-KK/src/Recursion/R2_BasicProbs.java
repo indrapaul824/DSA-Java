2a21278bd52a12f43c22ff810cdfe59f05ed9daa
IndraP24 <indrashis985@gmail.com>
2022-12-16 22:27:33 +0530

    Add Recursion basic problems

--- DSA-KK/src/Recursion/R2_BasicProbs.java
+++ DSA-KK/src/Recursion/R2_BasicProbs.java
@@ -2,12 +2,13 @@ package Recursion;
 
 import java.util.*;
 
-public class Q1_BasicProbs {
+public class R2_BasicProbs {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         name(1, n);
-        linear_1toN(1, n);
+        linear_1toN_1(1, n);
+        linear_1toN_2(1, n);
         linear_Nto1_1(1, n);
         linear_Nto1_2(1, n);
     }
@@ -19,11 +20,19 @@ public class Q1_BasicProbs {
         name(i+1, n);
     }
 
-    static void linear_1toN(int i, int n) {
+    static void linear_1toN_1(int i, int n) {
         if (i > n)
             return;
         System.out.println(i);
-        linear_1toN(i+1, n);
+        linear_1toN_1(i+1, n);
+    }
+
+    // Backtracking of previous recursion
+    static void linear_1toN_2(int i, int n) {
+        if (i > n)
+            return;
+        linear_1toN_2(i, n-1);
+        System.out.println(n);
     }
 
     static void linear_Nto1_1(int i, int n) {
@@ -33,6 +42,7 @@ public class Q1_BasicProbs {
         System.out.println(i);
     }
 
+    // Backtracking of previous recursion
     static void linear_Nto1_2(int i, int n) {
         if (i > n)
             return;
