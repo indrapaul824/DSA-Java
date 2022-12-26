8ea30e250e06c85c4506346642a414c2f18edf55
IndraP24 <indrashis985@gmail.com>
2022-12-26 23:50:01 +0530

    Update Bit Manipulation Intro

--- DSA-KK/src/Bit_Manipulation/B1_Intro.java
+++ DSA-KK/src/Bit_Manipulation/B1_Intro.java
@@ -1,8 +1,33 @@
 package Bit_Manipulation;
 
+import utils.Main.*;
+
+// Given a 32-bit unsigned integer num and an integer i. Perform following operations on the number -
+//
+// 1. Get ith bit
+// 2. Set ith bit
+// 3. Clear ith bit
+
 public class B1_Intro {
     public static void main(String[] args) {
-
+        FastReader in = new FastReader();
+        int T = in.nextInt();
+        while (T-- > 0) {
+            int n = in.nextInt();
+            int i = in.nextInt();
+            bitManipulation(n, i);
+            System.out.println();
+        }
     }
 
+    private static void bitManipulation(long n, long i) {
+        i = i - 1;
+        long pos = 1L << i;
+        long set = ((n & pos) >> i);
+        System.out.print(set + " ");
+        long seti = (n | pos);
+        System.out.print(seti + " ");
+        long clear = (n & (~pos));
+        System.out.print(clear);
+    }
 }
