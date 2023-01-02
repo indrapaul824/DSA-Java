32a3e7f41f9694b26169b568d7a225c1f4f09346
IndraP24 <indrashis985@gmail.com>
2023-01-02 23:54:51 +0530

    Add max product sum solution with printing elements

--- DSA-KK/src/Practice/Algorithms/MaxProdSubArray.java
+++ DSA-KK/src/Practice/Algorithms/MaxProdSubArray.java
@@ -1,8 +1,20 @@
 package Practice.Algorithms;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class MaxProdSubArray {
+    static class Pair {
+        int max;
+        ArrayList<Integer> arrayList;
+
+        public Pair(int max, ArrayList<Integer> arrayList) {
+            this.max = max;
+            this.arrayList = arrayList;
+        }
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Enter the size of the array: ");
@@ -12,23 +24,28 @@ public class MaxProdSubArray {
 
         for (int i = 0; i < n; i++)
             arr[i] = in.nextInt();
-
-        System.out.println("The maximum product of a sub-array is: " + maxProd(arr, n));
+        Pair pair = maxProd(arr, n);
+        System.out.println("The maximum product of a sub-array is: " + pair.max);
+        System.out.println("The sub array with max product is: " + pair.arrayList);
     }
 
-    public static int maxProd(int[] arr, int n) {
+    public static Pair maxProd(int[] arr, int n) {
+        ArrayList<Integer> list = new ArrayList<>();
         int max = 0, prod = 1;
 
         for (int i = 0; i < n; i++) {
             prod *= arr[i];
-
-            if (prod > max)
+            if (prod > max) {
                 max = prod;
+                list.add(arr[i]);
+            }
 
-            if (prod == 0)
+            if (prod == 0) {
                 prod = 1;
+                list.clear();
+            }
         }
 
-        return max;
+        return new Pair(max, list);
     }
 }
