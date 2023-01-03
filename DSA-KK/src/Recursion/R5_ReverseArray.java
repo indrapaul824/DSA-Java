7a02644124e7198a98dcf1e2f0fcac06ada2016b
IndraP24 <indrashis985@gmail.com>
2023-01-03 23:32:00 +0530

    Add Parameterized way for array reversal

---
+++ DSA-KK/src/Recursion/R5_ReverseArray.java
@@ -0,0 +1,34 @@
+package Recursion;
+
+import utils.Main.*;
+
+import java.util.Arrays;
+
+public class R5_ReverseArray {
+    public static void main(String[] args) {
+        FastReader in = new FastReader();
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        reverse(arr, 0, n-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    // Using 2 pointers - left and right
+    static void reverse(int[] arr, int l, int r) {
+        if (l >= r)
+            return;
+        swap(arr, l, r);
+        reverse(arr, l+1, r-1);
+    }
+
+    // Using 1 pointer
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
