2849712975ed7305e45cf6bd749c234259a48a1f
IndraP24 <indrashis985@gmail.com>
2023-01-03 23:31:44 +0530

    Update Parameterized way for factorial

--- DSA-KK/src/Recursion/R4_Factorial.java
+++ DSA-KK/src/Recursion/R4_Factorial.java
@@ -1,9 +1,25 @@
 package Recursion;
+import utils.Main.*;
 
 public class R4_Factorial {
     public static void main(String[] args) {
-        System.out.println(Factorial(3));
+        FastReader in = new FastReader();
+        int n = in.nextInt();
+        Factorial(n, 1);
+        System.out.println(Factorial(n));
     }
+
+    // Parameterised Way
+    static void Factorial(int i, int prod) {
+        if (i <= 1) {
+            System.out.println(prod);
+            return;
+        }
+
+        Factorial(i-1, prod * i);
+    }
+
+    // Functional Way
     static int Factorial(int n) {
         if (n == 1) {
             return 1;
