6d56dffaac5805290bb6cedf7114ff3bec38732b
IndraP24 <indrashis985@gmail.com>
2023-01-04 23:35:02 +0530

    Update Triplet num solution

--- DSA-KK/src/Practice/Main.java
+++ DSA-KK/src/Practice/Main.java
@@ -1,8 +1,23 @@
 package Practice;
 
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        Object obj = new Object();
-        System.out.println(obj.getClass());
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner in = new Scanner(System.in);
+        int t = 1;
+
+        while (t-- > 0) {
+            int a = 2, b = 5;
+            int div = 1;
+            for (int i = 1; i <= Math.max(a, b); i++) {
+                if (a%i != 0 || b%i != 0) {
+                    div = i - 1;
+                    break;
+                }
+            }
+            System.out.println(div);
+        }
     }
 }
