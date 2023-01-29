9568bc053b6c12810b74f73b9ddb558b1b884007
IndraP24 <indrashis985@gmail.com>
2023-01-29 21:19:07 +0530

    Add Palindrome check using Recursion

---
+++ DSA-KK/src/Recursion/R6_PalindromeCheck.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import utils.Main.*;
+public class R6_PalindromeCheck {
+    public static void main(String[] args) {
+        FastReader in = new FastReader();
+        String s = in.nextLine();
+        System.out.println(checkPalin(0, s));
+    }
+
+    static boolean checkPalin(int i, String s) {
+        if (i >= s.length()-1)
+            return true;
+        if (s.charAt(i) != s.charAt(s.length() - i - 1))
+            return false;
+
+        return checkPalin(i+1, s);
+    }
+}
