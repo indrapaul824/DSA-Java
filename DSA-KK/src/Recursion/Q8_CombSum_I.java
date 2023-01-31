44f2ec1f74499db5ebb58abb27061c3427908530
IndraP24 <indrashis985@gmail.com>
2023-01-31 09:39:11 +0530

    Add combination sum solution

---
+++ DSA-KK/src/Recursion/Q8_CombSum_I.java
@@ -0,0 +1,39 @@
+package Recursion;
+
+import utils.Main.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q8_CombSum_I {
+    public static void main(String[] args) {
+        FastReader in = new FastReader();
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int target = in.nextInt();
+        System.out.println(combSum(arr, target));
+    }
+
+    private static List<List<Integer>> combSum(int[] arr, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        findCombs(0, arr, target, res, new ArrayList<>());
+        return res;
+    }
+
+    private static void findCombs(int i, int[] arr, int target, List<List<Integer>> res, ArrayList<Integer> ds) {
+        if (i == arr.length) {
+            if (target == 0)
+                res.add(new ArrayList<>(ds));
+            return;
+        }
+        if (arr[i] <= target) {
+            ds.add(arr[i]);
+            findCombs(i, arr, target-arr[i], res, ds);
+            ds.remove(ds.size()-1);
+        }
+        findCombs(i+1, arr, target, res, ds);
+    }
+}
