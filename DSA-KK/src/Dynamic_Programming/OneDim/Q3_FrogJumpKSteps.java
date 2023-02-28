83993a5bfbc2743a1173ddee8932df4e376f29f4
IndraP24 <indrashis985@gmail.com>
2023-03-01 00:33:03 +0530

    Update FrogJumpKSteps solution

--- DSA-KK/src/Dynamic_Programming/OneDim/Q3_FrogJumpKSteps.java
+++ DSA-KK/src/Dynamic_Programming/OneDim/Q3_FrogJumpKSteps.java
@@ -14,29 +14,63 @@ public class Q3_FrogJumpKSteps {
         System.out.println(minEnergy(arr, n, k));
         System.out.println(minEnergy_mem(arr, n, k));
         System.out.println(minEnergy_tab(arr, n, k));
-        System.out.println(minEnergy_so(arr, n, k));
     }
 
+    // Recursive Approach
     private static int minEnergy(int[] arr, int n, int k) {
-        return frogJump(arr, n-1);
+        return frogJump(arr, n-1, k);
     }
-    private static int frogJump(int[] arr, int ind) {
+    private static int frogJump(int[] arr, int ind, int k) {
         if (ind == 0)
             return 0;
-        int left = frogJump(arr, ind - 1) + abs(arr[ind] - arr[ind - 1]);
-        int right = Integer.MAX_VALUE;
-        if (ind > 1)
-            right = frogJump(arr, ind - 2) + abs(arr[ind] - arr[ind - 2]);
+        int min_cost = Integer.MAX_VALUE;
+        for (int j = 1; j <= k; j++) {
+            if (ind - j >= 0) {
+                int jump = frogJump(arr, ind - j, k) +
+                        abs(arr[ind] - arr[ind - j]);
+                min_cost = min(min_cost, jump);
+            }
+        }
+        return min_cost;
+    }
 
-        return min(left, right);
-    }
+    // DP-1: Memoization
     private static int minEnergy_mem(int[] arr, int n, int k) {
-        return 0;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        return frogJump(arr, n-1, dp, k);
     }
+    private static int frogJump(int[] arr, int ind, int[] dp, int k) {
+        if (ind == 0)
+            return 0;
+        if (dp[ind] != -1)
+            return dp[ind];
+        int min_cost = Integer.MAX_VALUE;
+        for (int j = 1; j <= k; j++) {
+            if (ind - j >= 0) {
+                int jump = frogJump(arr, ind - j, dp, k) +
+                        abs(arr[ind] - arr[ind - j]);
+                min_cost = min(min_cost, jump);
+            }
+        }
+        return dp[ind] = min_cost;
+    }
+
+
+    // DP-2: Tabulation
     private static int minEnergy_tab(int[] arr, int n, int k) {
-        return 0;
-    }
-    private static int minEnergy_so(int[] arr, int n, int k) {
-        return 0;
+        int[] dp = new int[n];
+        dp[0] = 0;
+        for (int i = 1; i < n; i++) {
+            int min_cost = Integer.MAX_VALUE;
+            for (int j = 1; j <= k; j++) {
+                if (i - j >= 0) {
+                    int jump = dp[i - j] + abs(arr[i] - arr[i - j]);
+                    min_cost = min(min_cost, jump);
+                }
+            }
+            dp[i] = min_cost;
+        }
+        return dp[n-1];
     }
 }
