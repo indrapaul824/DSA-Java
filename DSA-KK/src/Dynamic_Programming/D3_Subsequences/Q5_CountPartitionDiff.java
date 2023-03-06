d0c38bcedc9890ab2ccb37759be0c15e430c8d99
IndraP24 <indrashis985@gmail.com>
2023-03-07 02:40:42 +0530

    Add Count Partition Min Sum Difference DP solution

---
+++ DSA-KK/src/Dynamic_Programming/D3_Subsequences/Q5_CountPartitionDiff.java
@@ -0,0 +1,41 @@
+package Dynamic_Programming.D3_Subsequences;
+
+// https://practice.geeksforgeeks.org/problems/partitions-with-given-difference/1
+import java.util.*;
+
+public class Q5_CountPartitionDiff {
+    public int countPartitions(int n, int d, int[] arr) {
+        int sum = 0;
+        for (int i : arr)
+            sum += i;
+        sum -= d;
+        if (sum < 0 || sum % 2 != 0)
+            return 0;
+        return perfectSum_so(arr, n, sum/2);
+    }
+    static int mod = (int)1e9+7;
+    public static int perfectSum_so(int[] arr,int n, int sum) {
+        int[] dp = new int[sum+1];
+
+        if (arr[0] == 0)
+            dp[0] = 2;
+        else
+            dp[0] = 1;
+        if (arr[0] != 0 && arr[0] <= sum)
+            dp[arr[0]] = 1;
+
+        for (int i = 1; i < n; i++) {
+            int[] curr = new int[sum+1];
+            for (int j = 0; j <= sum; j++) {
+                int not_pick = dp[j];
+                int pick = 0;
+                if (j >= arr[i] && j-arr[i] >= 0)
+                    pick = dp[j-arr[i]];
+
+                curr[j] = (pick + not_pick) % mod;
+            }
+            dp = curr;
+        }
+        return dp[sum];
+    }
+}
