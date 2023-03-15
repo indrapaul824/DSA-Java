adfdb467ba1ca105e607761d718690d84c3dc9b0
IndraP24 <indrashis985@gmail.com>
2023-03-15 08:31:54 +0530

    Update Tabulation and Space Opt solutions

--- DSA-KK/src/Dynamic_Programming/D4_Strings/Q1_LCS.java
+++ DSA-KK/src/Dynamic_Programming/D4_Strings/Q1_LCS.java
@@ -53,7 +53,44 @@ public class Q1_LCS {
     // DP-2: Tabulation
     public int longestCommonSubsequence_tab(String text1, String text2) {
         int n = text1.length(), m = text2.length();
-        int[][] dp = new int[n][m];
-        return 0;
+        int[][] dp = new int[n+1][m+1];
+        // Base Case
+        // for (int i = 0; i <= n; i++) dp[i][0] = 0;
+        // for (int j = 0; j <= m; i++) dp[0][j] = 0;
+
+        // Bottom-Up Approach
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (text1.charAt(i-1) == text2.charAt(j-1))
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    // DP-3: Space Optimization
+    public int longestCommonSubsequence_so(String text1, String text2) {
+        int n = text1.length(), m = text2.length();
+        int[] dp = new int[m+1];
+        // Base Case
+        // for (int i = 0; i <= n; i++) dp[i][0] = 0;
+        // for (int j = 0; j <= m; i++) dp[0][j] = 0;
+
+        // Bottom-Up Approach
+        for (int i = 1; i <= n; i++) {
+            int[] curr = new int[m+1];
+            for (int j = 1; j <= m; j++) {
+                if (text1.charAt(i-1) == text2.charAt(j-1))
+                    curr[j] = 1 + dp[j-1];
+                else
+                    curr[j] = Math.max(dp[j], curr[j-1]);
+            }
+            dp = curr;
+        }
+
+        return dp[m];
     }
 }
