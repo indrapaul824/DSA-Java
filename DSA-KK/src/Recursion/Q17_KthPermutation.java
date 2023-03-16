389ef3311d0be47e53e99408401ff5e0dadafca6
IndraP24 <indrashis985@gmail.com>
2023-03-16 22:29:48 +0530

    Add Kth Permutation solution

---
+++ DSA-KK/src/Recursion/Q17_KthPermutation.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class Q17_KthPermutation {
+    public String getPermutation(int n, int k) {
+        StringBuilder s = new StringBuilder();
+        StringBuilder st = new StringBuilder();
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            s.append(i);
+            fact *= i;
+        }
+        fact /= n;
+        k--;
+        while(true) {
+            st.append(s.charAt(k/fact));
+            s.deleteCharAt(k/fact);
+            if (s.length() == 0)
+                break;
+            k = k % fact;
+            fact /= s.length();
+        }
+        return st.toString();
+    }
+}
