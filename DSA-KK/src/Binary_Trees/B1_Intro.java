63bfbef70fe9031c7c86ccdf01ff18483557f97b
IndraP24 <indrashis985@gmail.com>
2023-03-22 14:43:43 +0530

    Add Binary Tree intro code

--- DSA-KK/src/Binary_Trees/B1_Intro.java
+++ DSA-KK/src/Binary_Trees/B1_Intro.java
@@ -1,47 +1,114 @@
 package Binary_Trees;
 
-import java.util.Scanner;
+import java.util.*;
 
-public class B1_Intro {
-    static class Node {
-        int data;
-        Node left, right;
+class Node<T extends Comparable<?>> {
+    Node<T> left, right;
+    T data;
 
-        public Node(int data) {
-            this.data = data;
-            left = right = null;
-        }
+    public Node(T data) {
+        this.data = data;
+    }
+}
+
+
+class BTreePrinter {
+
+    public static <T extends Comparable<?>> void printNode(Node<T> root) {
+        int maxLevel = BTreePrinter.maxLevel(root);
+
+        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
     }
 
-    private Node root;
-
-    public static void inOrder(Node temp) {
-        if (temp == null)
+    private static <T extends Comparable<?>> void printNodeInternal(List<Node<T>> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || BTreePrinter.isAllElementsNull(nodes))
             return;
 
-        inOrder(temp.left);
-        System.out.print(temp.data + " -> ");
-        inOrder(temp.right);
+        int floor = maxLevel - level;
+        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+        BTreePrinter.printWhitespaces(firstSpaces);
+
+        List<Node<T>> newNodes = new ArrayList<Node<T>>();
+        for (Node<T> node : nodes) {
+            if (node != null) {
+                System.out.print(node.data);
+                newNodes.add(node.left);
+                newNodes.add(node.right);
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+
+            BTreePrinter.printWhitespaces(betweenSpaces);
+        }
+        System.out.println();
+
+        for (int i = 1; i <= endgeLines; i++) {
+            for (Node<T> node : nodes) {
+                BTreePrinter.printWhitespaces(firstSpaces - i);
+                if (node == null) {
+                    BTreePrinter.printWhitespaces(endgeLines + endgeLines + i + 1);
+                    continue;
+                }
+
+                if (node.left != null)
+                    System.out.print("/");
+                else
+                    BTreePrinter.printWhitespaces(1);
+
+                BTreePrinter.printWhitespaces(i + i - 1);
+
+                if (node.right != null)
+                    System.out.print("\\");
+                else
+                    BTreePrinter.printWhitespaces(1);
+
+                BTreePrinter.printWhitespaces(endgeLines + endgeLines - i);
+            }
+
+            System.out.println();
+        }
+
+        printNodeInternal(newNodes, level + 1, maxLevel);
     }
 
-    public Node buildTree() {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        if (n == -1)
-            return null;
-
-        root = new Node(n);
-        root.left = buildTree();
-        root.right = buildTree();
-
-        return root;
+    private static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++)
+            System.out.print(" ");
     }
 
+    private static <T extends Comparable<?>> int maxLevel(Node<T> node) {
+        if (node == null)
+            return 0;
+
+        return Math.max(BTreePrinter.maxLevel(node.left), BTreePrinter.maxLevel(node.right)) + 1;
+    }
+
+    private static <T> boolean isAllElementsNull(List<T> list) {
+        for (Object object : list) {
+            if (object != null)
+                return false;
+        }
+
+        return true;
+    }
+
+}
+
+public class B1_Intro {
     public static void main(String[] args) {
-        B1_Intro tree = new B1_Intro();
+        Node<Integer> root = new Node<>(1);
+        root.left = new Node<>(2);
+        root.right = new Node<>(3);
+        root.left.left = new Node<>(4);
+        root.left.right = new Node<>(5);
+        root.right.left = new Node<>(6);
+        root.right.right = new Node<>(7);
 
-        tree.root = tree.buildTree();
-
-        inOrder(tree.root);
+        BTreePrinter.printNode(root);
     }
 }
