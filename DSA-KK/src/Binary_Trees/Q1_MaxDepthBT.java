8c2cfcdbf6756e0f75c7bbab78b960d415276794
IndraP24 <indrashis985@gmail.com>
2023-03-27 22:18:45 +0530

    Add Max Height of BT

---
+++ DSA-KK/src/Binary_Trees/Q1_MaxDepthBT.java
@@ -0,0 +1,28 @@
+package Binary_Trees;
+
+//  https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+public class Q1_MaxDepthBT {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Recursive Solution
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+
+        return 1 + Math.max(l, r);
+    }
+}
