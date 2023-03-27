2647e6710bd33d3567df4388af3686d31777306c
IndraP24 <indrashis985@gmail.com>
2023-03-27 22:20:14 +0530

    Add Identical BT check

---
+++ DSA-KK/src/Binary_Trees/Q5_IdenticalBT.java
@@ -0,0 +1,16 @@
+package Binary_Trees;
+
+public class Q5_IdenticalBT {
+    static class Node{
+        int data;
+        Node left,right;
+        Node(int d){
+            data=d;
+            left=right=null;
+        }
+    }
+
+    boolean isIdentical(Node root1, Node root2) {
+        return false;
+    }
+}
