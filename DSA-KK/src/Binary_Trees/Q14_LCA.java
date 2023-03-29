97761ffbfdbf77ab024b609ca9c90600d9e4546a
IndraP24 <indrashis985@gmail.com>
2023-03-29 23:00:27 +0530

    Update LCA solution with Problem links

--- DSA-KK/src/Binary_Trees/Q14_LCA.java
+++ DSA-KK/src/Binary_Trees/Q14_LCA.java
@@ -1,5 +1,8 @@
 package Binary_Trees;
 
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+// https://practice.geeksforgeeks.org/problems/lowest-common-ancestor-in-a-binary-tree/1
+
 public class Q14_LCA {
     static class Node {
         int data;
@@ -16,6 +19,21 @@ public class Q14_LCA {
 
         Node left = lca(root.left, n1, n2);
         Node right = lca(root.right, n1, n2);
+
+        if (left == null)
+            return right;
+        else if (right == null)
+            return left;
+        else
+            return root;
+    }
+
+    public Node lowestCommonAncestor(Node root, Node p, Node q) {
+        if (root == null || root == p || root == q)
+            return root;
+
+        Node left = lowestCommonAncestor(root.left, p, q);
+        Node right = lowestCommonAncestor(root.right, p, q);
 
         if (left == null)
             return right;
