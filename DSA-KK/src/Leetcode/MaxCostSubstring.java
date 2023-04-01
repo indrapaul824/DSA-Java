225f64ef40bd8054831108883994cbc67e139b94
IndraP24 <indrashis985@gmail.com>
2023-04-01 21:31:07 +0530

    Add Max Cost Substring solution

---
+++ DSA-KK/src/Leetcode/MaxCostSubstring.java
@@ -0,0 +1,20 @@
+package Leetcode;
+
+public class MaxCostSubstring {
+    public int maximumCostSubstring(String s, String chars, int[] vals) {
+        int max = 0;
+        int sum = 0;
+        for (int j = 0; j < s.length(); j++) {
+            char t = s.charAt(j);
+            int n = chars.indexOf(t);
+            if (n == -1)
+                sum += (int)t - 96;
+            else
+                sum += vals[n];
+            if (sum < 0)
+                sum = 0;
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
