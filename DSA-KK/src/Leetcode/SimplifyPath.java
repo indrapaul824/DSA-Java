9dd8e6ae1fc1480d895033315114ecacdf9a2c01
IndraP24 <indrashis985@gmail.com>
2023-04-12 22:31:13 +0530

    Add simplify paths solution

---
+++ DSA-KK/src/Leetcode/SimplifyPath.java
@@ -0,0 +1,28 @@
+package Leetcode;
+import java.util.*;
+
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+        var st = new ArrayDeque<String>();
+
+        String[] p = path.split("/");
+
+        for (String s : p) {
+            System.out.println(s);
+            if (!st.isEmpty() && s.equals(".."))
+                st.pop();
+            else if (!s.equals("") && !s.equals(".") && !s.equals(".."))
+                st.push(s);
+        }
+
+        if (st.isEmpty())
+            return "/";
+        StringBuilder sb = new StringBuilder();
+        sb.append("/");
+        while (!st.isEmpty())
+            sb.append(st.pollLast()).append("/");
+        sb.deleteCharAt(sb.length()-1);
+
+        return sb.toString();
+    }
+}
