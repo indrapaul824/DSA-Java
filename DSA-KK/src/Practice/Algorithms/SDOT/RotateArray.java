39b024abb1794832bbf3c72944a70133654c78b9
IndraP24 <indrashis985@gmail.com>
2023-04-15 22:41:05 +0530

    Add Rotate Array solution

---
+++ DSA-KK/src/Practice/Algorithms/SDOT/RotateArray.java
@@ -0,0 +1,16 @@
+package Practice.Algorithms.SDOT;
+
+import java.util.Arrays;
+import java.util.*;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 2;
+        System.out.println(Arrays.toString(arr));
+//        rotateArray(arr, k);
+        System.out.println(Arrays.toString(arr));
+        Map<Character, Integer> map = new HashMap<>();
+
+    }
+}
