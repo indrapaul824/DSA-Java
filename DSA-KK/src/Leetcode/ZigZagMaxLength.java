eea8520cf4e94cfd8a03c5045955dfb7e81d1916
IndraP24 <indrashis985@gmail.com>
2023-04-19 22:58:25 +0530

    Add ZigZag max length

---
+++ DSA-KK/src/Leetcode/ZigZagMaxLength.java
@@ -0,0 +1,52 @@
+package Leetcode;
+import java.util.*;
+public class ZigZagMaxLength {
+    /**
+     * Definition for a binary tree node.
+     */
+     public static class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+    static class Solution {
+        static class Triplet {
+            TreeNode node;
+            int n;
+            Boolean left;
+
+            Triplet(TreeNode node, int n, Boolean left) {
+                this.node = node;
+                this.n = n;
+                this.left = left;
+            }
+
+        }
+        public int longestZigZag(TreeNode root) {
+            var st = new ArrayDeque<Triplet>();
+            int max = 0;
+            st.push(new Triplet(root, 0, null));
+
+            while (!st.isEmpty()) {
+                Triplet t = st.pop();
+                TreeNode node = t.node;
+                int n = t.n;
+                Boolean left = t.left;
+                if (node != null) {
+                    max = Math.max(max, n);
+                    st.push(new Triplet(node.left, left != null && !left ? n + 1 : 1, true));
+                    st.push(new Triplet(node.right, left != null && left ? n + 1 : 1, false));
+                }
+            }
+
+            return max;
+        }
+    }
+}
