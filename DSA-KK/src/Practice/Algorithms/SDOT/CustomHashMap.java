6b2b0adc6859fdf69453b05e1c3e9eb1806d15f9
IndraP24 <indrashis985@gmail.com>
2023-04-22 23:21:47 +0530

    Add CustomHashMap solution

---
+++ DSA-KK/src/Practice/Algorithms/SDOT/CustomHashMap.java
@@ -0,0 +1,22 @@
+package Practice.Algorithms.SDOT;
+
+public class CustomHashMap<K, V> {
+
+    public
+    static class Entry<K, V>  {
+        private K key;
+        private V value;
+        private Entry<K, V> next;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public Entry(K key, V value, Entry<K, V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
