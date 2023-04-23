4c12db8974e5fdffc8ecdfc6488aab7c2af4d073
IndraP24 <indrashis985@gmail.com>
2023-04-23 11:07:57 +0530

    Update Target Occurrence solution

--- DSA-KK/src/Practice/Algorithms/SDOT/TargetOccurrence.java
+++ DSA-KK/src/Practice/Algorithms/SDOT/TargetOccurrence.java
@@ -1,4 +1,27 @@
 package Practice.Algorithms.SDOT;
+import utils.Main.*;
+import java.util.*;
 
 public class TargetOccurrence {
+    public static void main(String[] args) {
+        FastReader in = new FastReader();
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int target = in.nextInt();
+        List<Integer> list = new ArrayList<>();
+        targetOcc(0, arr, target, list);
+        System.out.println(list);
+    }
+
+    private static void targetOcc(int i, int[] arr, int target, List<Integer> list) {
+        if (i == arr.length)
+            return;
+        if (arr[i] == target)
+            list.add(i);
+
+        targetOcc(i+1, arr, target, list);
+    }
 }
