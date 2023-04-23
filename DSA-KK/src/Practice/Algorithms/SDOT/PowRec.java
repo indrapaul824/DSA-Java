e7833eeab80d390054e30b9f443a2d12ae859268
IndraP24 <indrashis985@gmail.com>
2023-04-23 11:08:30 +0530

    Update Power of a num recursive solution

--- DSA-KK/src/Practice/Algorithms/SDOT/PowRec.java
+++ DSA-KK/src/Practice/Algorithms/SDOT/PowRec.java
@@ -1,7 +1,17 @@
 package Practice.Algorithms.SDOT;
 import utils.Main.*;
+
 public class PowRec {
     public static void main(String[] args) {
+        FastReader in = new FastReader();
+        int n = in.nextInt();
+        int x = in.nextInt();
+        System.out.println(power(n, x));
+    }
 
+    private static int power(int n, int x) {
+        if (x == 0)
+            return 1;
+        return n * power(n, x-1);
     }
 }
