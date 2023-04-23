728ecb94781d53693415bf8bdecd8d481393188f
IndraP24 <indrashis985@gmail.com>
2023-04-23 12:11:43 +0530

    Add Weight Airplane solution

---
+++ DSA-KK/src/Practice/Algorithms/SDOT/WeightAirplane.java
@@ -0,0 +1,36 @@
+package Practice.Algorithms.SDOT;
+import utils.Main.*;
+public class WeightAirplane {
+    public static void main(String[] args) {
+        FastReader in = new FastReader();
+        int n = in.nextInt();
+        int t = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println("Total Amount: " + weightMachine(arr, n, t));
+    }
+
+    private static int weightMachine(int[] arr, int n, int t) {
+        int amount = 0;
+        for (int i : arr) {
+            if (i <= t)
+                amount += 1;
+            else
+                amount += 2;
+        }
+        return amount;
+    }
+
+    private static int weightMachine_II(int[] arr, int n, int t) {
+        int sum = 0;
+        for (int i : arr)
+            if (i <= t)
+                sum += 1;
+            else
+                sum += i - t;
+        return sum;
+    }
+}
