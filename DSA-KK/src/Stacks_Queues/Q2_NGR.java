5965fa5c9cdbd8e3a06a47509d65f905ebd5b3d2
IndraP24 <indrashis985@gmail.com>
2023-04-27 23:06:22 +0530

    Update NGR solution

--- DSA-KK/src/Stacks_Queues/Q2_NGR.java
+++ DSA-KK/src/Stacks_Queues/Q2_NGR.java
@@ -39,6 +39,22 @@ public class Q2_NGR {
         return ans;
     }
 
+    public static long[] nextLargerElement(long[] arr, int n) {
+        Stack<Long> st = new Stack<>();
+        long[] res = new long[n];
+        for (int i = n-1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() <= arr[i])
+                st.pop();
+            if (st.isEmpty())
+                res[i] = -1;
+            else
+                res[i] = st.peek();
+            st.push(arr[i]);
+        }
+
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 3, 4, 2};
         int[] nums = {4, 1, 2};
