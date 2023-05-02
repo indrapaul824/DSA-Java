abd1726d53fbd8d29c3466327b0e049ca1ec7101
IndraP24 <indrashis985@gmail.com>
2023-05-02 10:36:25 +0530

    Add BSTIterator optimal solution

--- DSA-KK/src/Binary_Search_Trees/Q13_BSTIterator.java
+++ DSA-KK/src/Binary_Search_Trees/Q13_BSTIterator.java
@@ -2,6 +2,7 @@ package Binary_Search_Trees;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Q13_BSTIterator {
     public static class TreeNode {
@@ -15,6 +16,8 @@ public class Q13_BSTIterator {
             this.right = right;
         }
     }
+
+    // Approach 1: InOrder store and traverse
     List<Integer> list = new ArrayList<>();
     int curr;
     public Q13_BSTIterator(TreeNode root) {
@@ -35,5 +38,45 @@ public class Q13_BSTIterator {
 
     public boolean hasNext() {
         return (curr+1) < list.size();
+    }
+}
+
+class BSTIt {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    // Approach 2: Optimal using BST
+    Stack<TreeNode> st;
+    public BSTIt(TreeNode root) {
+        st = new Stack<>();
+        while (root != null) {
+            st.push(root);
+            root = root.left;
+        }
+    }
+
+    public int next() {
+        TreeNode curr = st.pop();
+        int data = curr.val;
+        if (curr.right != null) {
+            curr = curr.right;
+            while (curr != null) {
+                st.push(curr);
+                curr = curr.left;
+            }
+        }
+        return data;
+    }
+
+    public boolean hasNext() {
+        return (!st.isEmpty());
     }
 }
