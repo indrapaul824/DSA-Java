a20b3643924e6720e5c971ee2f7abddf3b6ffc93
IndraP24 <indrashis985@gmail.com>
2023-05-10 12:32:35 +0530

    Update BFS code

--- DSA-KK/src/Graphs/G2_BFS.java
+++ DSA-KK/src/Graphs/G2_BFS.java
@@ -1,4 +1,31 @@
 package Graphs;
+import java.util.*;
 
 public class G2_BFS {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Data Structures
+        Queue<Integer> q = new ArrayDeque<>();
+        boolean[] vis = new boolean[V];
+        ArrayList<Integer> bfs = new ArrayList<>();
+
+        // Initial Configuration
+        vis[0] = true;
+        q.offer(0);
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+
+            for (int i : adj.get(curr)) {
+                if (!vis[i]) {
+                    vis[i] = true;
+                    q.offer(i);
+                }
+            }
+
+            bfs.add(curr);
+        }
+
+        return bfs;
+    }
 }
