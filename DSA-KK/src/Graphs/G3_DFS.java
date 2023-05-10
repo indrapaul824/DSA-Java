49772709537a7d1373c2e1db094d5a9998bc0825
IndraP24 <indrashis985@gmail.com>
2023-05-10 23:42:59 +0530

    Update DFS Recursive code

--- DSA-KK/src/Graphs/G3_DFS.java
+++ DSA-KK/src/Graphs/G3_DFS.java
@@ -1,4 +1,21 @@
 package Graphs;
+import java.util.*;
 
 public class G3_DFS {
+    void dfs(int node, boolean[] vis, ArrayList<Integer> res, ArrayList<ArrayList<Integer>> adj) {
+        vis[node] = true;
+        res.add(node);
+
+        for (int i : adj.get(node)) {
+            if (!vis[i])
+                dfs(i, vis, res, adj);
+        }
+    }
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> res = new ArrayList<>();
+        boolean[] vis = new boolean[V];
+        dfs(0, vis, res, adj);
+        return res;
+    }
 }
