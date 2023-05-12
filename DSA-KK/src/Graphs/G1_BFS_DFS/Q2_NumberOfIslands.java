42f0f89397243eb823c2bc479fa88f91ebe8ae7f
IndraP24 <indrashis985@gmail.com>
2023-05-12 23:33:43 +0530

    Add Number of Islands DFS solution

--- DSA-KK/src/Graphs/G1_BFS_DFS/Q2_NumberOfIslands.java
+++ DSA-KK/src/Graphs/G1_BFS_DFS/Q2_NumberOfIslands.java
@@ -37,7 +37,15 @@ public class Q2_NumberOfIslands {
         }
     }
 
-
+    private void dfsFill(char[][] grid,int i, int j){
+        if(i>=0 && j>=0 && i<grid.length && j<grid[0].length && grid[i][j]=='1') {
+            grid[i][j]='0';
+            dfsFill(grid, i + 1, j);
+            dfsFill(grid, i - 1, j);
+            dfsFill(grid, i, j + 1);
+            dfsFill(grid, i, j - 1);
+        }
+    }
 
     public int numIslands(char[][] grid) {
         int n = grid.length, m = grid[0].length;
