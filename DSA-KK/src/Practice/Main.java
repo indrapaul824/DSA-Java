51bd381d43005f65551058acf71c171aca20557a
IndraP24 <indrashis985@gmail.com>
2023-05-14 22:02:37 +0530

    Update Main

--- DSA-KK/src/Practice/Main.java
+++ DSA-KK/src/Practice/Main.java
@@ -5,19 +5,30 @@ import java.util.*;
 public class Main {
     public static void main (String[] args) throws java.lang.Exception
     {
-        Scanner in = new Scanner(System.in);
-        int t = 1;
+        System.out.println(Arrays.toString(circularGameLosers(5, 2)));
+    }
 
-        while (t-- > 0) {
-            int a = 2, b = 5;
-            int div = 1;
-            for (int i = 1; i <= Math.max(a, b); i++) {
-                if (a%i != 0 || b%i != 0) {
-                    div = i - 1;
-                    break;
-                }
-            }
-            System.out.println(div);
+    public static int[] circularGameLosers(int n, int k) {
+        int[] cnt = new int[n];
+        int p = 1;
+
+        cnt[0] = 1;
+        int i = p*k % n;
+        while (true) {
+            cnt[i] += 1;
+
+            if (cnt[i] == 2)
+                break;
+            p++;
+            i += ((p*k) % n);
         }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int j = 0; j < n; j++) {
+            if (cnt[j] == 0)
+                list.add(j+1);
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
