2719d1e062eb1b0f07ec43921df14769306557b5
IndraP24 <indrashis985@gmail.com>
2023-05-16 22:20:13 +0530

    Add Swap Pair Nodes in LL solution

---
+++ DSA-KK/src/Leetcode/SwapPairNodesLinkedList.java
@@ -0,0 +1,42 @@
+package Leetcode;
+
+public class SwapPairNodesLinkedList {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        public ListNode() {}
+    }
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+
+        ListNode curr = head, dummy = new ListNode();
+        ListNode prev = dummy;
+
+        prev.next = head;
+
+        while (curr != null && curr.next != null) {
+            prev.next = curr.next;
+            curr.next = prev.next.next;
+            prev.next.next = curr;
+
+            prev = curr;
+            curr = curr.next;
+        }
+
+        head = dummy.next;
+
+        return head;
+    }
+}
