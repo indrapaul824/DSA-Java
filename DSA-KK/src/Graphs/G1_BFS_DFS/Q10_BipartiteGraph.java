d4146c7fc4d150f61ba18908db887a743a347a37
IndraP24 <indrashis985@gmail.com>
2023-05-25 16:13:31 +0530

    Add Bipartite Graph DFS solution

--- DSA-KK/src/Graphs/G1_BFS_DFS/Q10_BipartiteGraph.java
+++ DSA-KK/src/Graphs/G1_BFS_DFS/Q10_BipartiteGraph.java
@@ -2,6 +2,8 @@ package Graphs.G1_BFS_DFS;
 import java.util.*;
 
 public class Q10_BipartiteGraph {
+
+    // Using BFS
     private boolean check(int start, int[] color, int[][] graph) {
         Queue<Integer> Q = new ArrayDeque<>();
 
@@ -23,7 +25,7 @@ public class Q10_BipartiteGraph {
 
         return true;
     }
-    public boolean isBipartite(int[][] graph) {
+    public boolean isBipartite_bfs(int[][] graph) {
         int n = graph.length;
 
         int[] color = new int[n];
@@ -32,6 +34,36 @@ public class Q10_BipartiteGraph {
         for (int i = 0; i < n; i++) {
             if (color[i] == -1) {
                 if (!check(i, color, graph))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    // Using DFS
+    private boolean dfs(int node, int col, int[] color, int[][] graph) {
+        color[node] = col;
+
+        for (int ne : graph[node]) {
+            if (color[ne] == -1) {
+                if (!dfs(ne, 1 - col, color, graph))
+                    return false;
+            }
+            else if (color[ne] == col)
+                return false;
+        }
+
+        return true;
+    }
+    public boolean isBipartite_dfs(int[][] graph) {
+        int n = graph.length;
+
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                if (!dfs(i, 0, color, graph))
                     return false;
             }
         }
