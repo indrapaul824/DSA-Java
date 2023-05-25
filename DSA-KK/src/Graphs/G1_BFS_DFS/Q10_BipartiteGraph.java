7b14bd60eb172477803fc4c919d05dd154db4ed0
IndraP24 <indrashis985@gmail.com>
2023-05-25 16:03:52 +0530

    Add Bipartite Graph BFS solution

--- DSA-KK/src/Graphs/G1_BFS_DFS/Q10_BipartiteGraph.java
+++ DSA-KK/src/Graphs/G1_BFS_DFS/Q10_BipartiteGraph.java
@@ -1,4 +1,40 @@
 package Graphs.G1_BFS_DFS;
+import java.util.*;
 
 public class Q10_BipartiteGraph {
+    private boolean check(int start, int[] color, int[][] graph) {
+        Queue<Integer> Q = new ArrayDeque<>();
+
+        Q.offer(start);
+        color[start] = 0;
+
+        while (!Q.isEmpty()) {
+            int node = Q.poll();
+
+            for (int ne : graph[node]) {
+                if (color[ne] == -1) {
+                    color[ne] = 1 - color[node];
+                    Q.offer(ne);
+                }
+                else if (color[ne] == color[node])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                if (!check(i, color, graph))
+                    return false;
+            }
+        }
+        return true;
+    }
 }
