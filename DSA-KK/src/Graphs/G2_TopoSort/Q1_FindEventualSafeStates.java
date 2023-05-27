39e1e63b0291858dc469c27e19cd734a16de1f69
IndraP24 <indrashis985@gmail.com>
2023-05-27 18:51:02 +0530

    Add Find Eventual Safe state DFS solution

---
+++ DSA-KK/src/Graphs/G2_TopoSort/Q1_FindEventualSafeStates.java
@@ -0,0 +1,36 @@
+package Graphs.G2_TopoSort;
+import java.util.*;
+
+public class Q1_FindEventualSafeStates {
+    // Using Cycle Detection with DFS
+    private void dfs(int node, boolean[] vis, boolean[] pathVis, int[][] graph) {
+        vis[node] = true;
+        pathVis[node] = true;
+
+        for (int ne : graph[node]) {
+            if (!vis[ne])
+                dfs(ne, vis, pathVis, graph);
+            if (vis[ne] && pathVis[ne])
+                return;
+        }
+
+        pathVis[node] = false;
+    }
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int m = graph.length;
+
+        boolean[] vis = new boolean[m], pathVis = new boolean[m];
+
+        for (int i = 0; i < m; i++) {
+            if (!vis[i])
+                dfs(i, vis, pathVis, graph);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < m; i++)
+            if (!pathVis[i])
+                list.add(i);
+
+        return list;
+    }
+}
