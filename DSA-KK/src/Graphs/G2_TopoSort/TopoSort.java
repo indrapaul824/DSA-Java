53f69d9384e401fe074bf5476374cfdb68366f6e
IndraP24 <indrashis985@gmail.com>
2023-05-28 12:11:28 +0530

    Update Topological Sort BFS Algorithm

--- DSA-KK/src/Graphs/G2_TopoSort/TopoSort.java
+++ DSA-KK/src/Graphs/G2_TopoSort/TopoSort.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class TopoSort {
 
+    // Using DFS and Stack
     private static void dfs(int node, int[] vis, Stack<Integer> st, ArrayList<ArrayList<Integer>> adj) {
         vis[node] = 1;
 
@@ -33,4 +34,35 @@ public class TopoSort {
     }
 
 
+    // Using BFS - Kahn's Algorithm
+    static int[] topoSort_bfs(int V, ArrayList<ArrayList<Integer>> adj)
+    {
+        int[] inDegree = new int[V], topo = new int[V];
+        Queue<Integer> Q = new ArrayDeque<>();
+
+        for (int i = 0; i < V; i++) {
+            for (int ne : adj.get(i))
+                inDegree[ne]++;
+        }
+
+        for (int i = 0; i < V; i++) {
+            if (inDegree[i] == 0)
+                Q.offer(i);
+        }
+
+        int i = 0;
+        while (!Q.isEmpty()) {
+            int node = Q.poll();
+            topo[i++] = node;
+
+            for (int ne : adj.get(node)) {
+                inDegree[ne]--;
+
+                if (inDegree[ne] == 0)
+                    Q.offer(ne);
+            }
+        }
+
+        return topo;
+    }
 }
