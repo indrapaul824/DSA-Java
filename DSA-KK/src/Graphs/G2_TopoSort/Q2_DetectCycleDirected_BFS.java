d547c733add792c3fe34165cda934150853b9879
IndraP24 <indrashis985@gmail.com>
2023-05-29 22:15:45 +0530

    Update Directed Graph Cycle Detection initial commit

--- DSA-KK/src/Graphs/G2_TopoSort/Q2_DetectCycleDirected_BFS.java
+++ DSA-KK/src/Graphs/G2_TopoSort/Q2_DetectCycleDirected_BFS.java
@@ -1,5 +1,9 @@
 package Graphs.G2_TopoSort;
+import java.util.*;
 
 public class Q2_DetectCycleDirected_BFS {
+    // Function to detect cycle in a directed graph.
+    public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
 
+    }
 }
