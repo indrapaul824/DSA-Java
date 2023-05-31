b5ae9d68ed6b2924bea6dc6b0edd1339409a4b62
IndraP24 <indrashis985@gmail.com>
2023-05-31 23:29:45 +0530

    Add HashTable solution

---
+++ DSA-KK/src/Leetcode/HashTable.java
@@ -0,0 +1,38 @@
+package Leetcode;
+
+public class HashTable {
+    int[] num;
+    public HashTable() {
+        num = new int[31251];
+    }
+
+    // set the bit if that element is present
+    public void add(int key) {
+        num[getIdx(key)]|=getMask(key);
+    }
+
+    //unset the bit if a key is not present
+    public void remove(int key) {
+        num[getIdx(key)] &= (~getMask(key));
+    }
+
+    //check if a bit corresponding to the key is set or not
+    public boolean contains(int key) {
+        return (num[getIdx(key)]&getMask(key))!=0;
+    }
+
+    // idx of num[] to which this key belongs
+    // for key = 37, it will give 1
+    private int getIdx(int key)
+    {
+        return (key/32);
+    }
+
+    // get mask representing the bit inside num[idx] that corresponds to given key.
+    // for key = 37, it will give 00000000000000000000000000100000
+    private int getMask(int key)
+    {
+        key%=32;
+        return (1<<key);
+    }
+}
