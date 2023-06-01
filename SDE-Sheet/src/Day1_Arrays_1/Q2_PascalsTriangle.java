684babd60a044bb23e68f4fac5b48d88d981eab5
IndraP24 <indrashis985@gmail.com>
2023-06-01 17:40:09 +0530

    Add Pascal's Triangle optimal solution

--- SDE-Sheet/src/Day1_Arrays_1/Q2_PascalsTriangle.java
+++ SDE-Sheet/src/Day1_Arrays_1/Q2_PascalsTriangle.java
@@ -1,4 +1,27 @@
 package Day1_Arrays_1;
+import java.util.*;
 
 public class Q2_PascalsTriangle {
+    private List<Integer> generateRow(int r) {
+        int element = 1;
+        List<Integer> row = new ArrayList<>();
+        row.add(element);
+        for (int c = 1; c < r; c++) {
+            element *= r - c;
+            element /= c;
+            row.add(element);
+        }
+
+        return row;
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int r = 1; r <= numRows; r++) {
+            res.add(generateRow(r));
+        }
+
+        return res;
+    }
 }
