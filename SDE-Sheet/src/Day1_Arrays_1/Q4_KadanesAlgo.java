6eb6065ebfeb3274dd27dfe195fb09012a5c8cb9
IndraP24 <indrashis985@gmail.com>
2023-06-01 23:54:34 +0530

    Add Kadane's Algo

--- SDE-Sheet/src/Day1_Arrays_1/Q4_KadanesAlgo.java
+++ SDE-Sheet/src/Day1_Arrays_1/Q4_KadanesAlgo.java
@@ -1,4 +1,16 @@
 package Day1_Arrays_1;
 
 public class Q4_KadanesAlgo {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE, sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+            max = Math.max(max, sum);
+            if (sum < 0)
+                sum = 0;
+        }
+
+        return max;
+    }
 }
