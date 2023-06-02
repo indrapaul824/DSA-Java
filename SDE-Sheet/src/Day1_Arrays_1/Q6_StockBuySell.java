7ffcb89399753be70b9b72971f664f0335292aeb
IndraP24 <indrashis985@gmail.com>
2023-06-02 11:53:19 +0530

    Update Stock and Buy and Sell solution

--- SDE-Sheet/src/Day1_Arrays_1/Q6_StockBuySell.java
+++ SDE-Sheet/src/Day1_Arrays_1/Q6_StockBuySell.java
@@ -1,4 +1,15 @@
 package Day1_Arrays_1;
 
 public class Q6_StockBuySell {
+    public int maxProfit(int[] prices) {
+        int minBuy = prices[0], maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - minBuy;
+            maxProfit = Math.max(maxProfit, profit);
+            minBuy = Math.min(minBuy, prices[i]);
+        }
+
+        return maxProfit;
+    }
 }
