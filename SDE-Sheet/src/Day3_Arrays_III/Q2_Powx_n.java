6930cbcca3dc66924bd1cc59e8046041519585e0
IndraP24 <indrashis985@gmail.com>
2023-06-11 14:41:54 +0530

    Add Pow(x,n) optimal solution

--- SDE-Sheet/src/Day3_Arrays_III/Q2_Powx_n.java
+++ SDE-Sheet/src/Day3_Arrays_III/Q2_Powx_n.java
@@ -1,4 +1,29 @@
 package Day3_Arrays_III;
 
 public class Q2_Powx_n {
+    private double pow(double x, long n) {
+        double ans = 1.0;
+        while (n > 0) {
+            if (n%2 == 0) {
+                n /= 2;
+                x *= x;
+            }
+            else {
+                ans *= x;
+                n -= 1;
+            }
+        }
+
+        return ans;
+    }
+    public double myPow(double x, int n) {
+        long N = (long) n;
+        if (n < 0)
+            N = -1 * N;
+
+        if (n < 0)
+            return (double)1 / pow(x, N);
+
+        return pow(x, N);
+    }
 }
