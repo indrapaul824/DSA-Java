51d54bb91decc6a07025eee7567bfe2d424cca7d
IndraP24 <indrashis985@gmail.com>
2023-06-11 21:31:09 +0530

    Add Majority Element better and optimal solution

--- SDE-Sheet/src/Day3_Arrays_III/Q3_MajorityElement.java
+++ SDE-Sheet/src/Day3_Arrays_III/Q3_MajorityElement.java
@@ -1,4 +1,33 @@
 package Day3_Arrays_III;
+import java.util.*;
 
 public class Q3_MajorityElement {
+    // Better
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+            if (map.get(n) > nums.length/2)
+                return n;
+        }
+
+        return -1;
+    }
+
+    // Optimal - Moore's Voting Algo
+    public int majorityElement_opti(int[] nums) {
+        int cnt = 0, ele = 0;
+
+        for (int n : nums) {
+            if (cnt == 0)
+                ele = n;
+            if (ele == n)
+                cnt++;
+            else
+                cnt = 0;
+        }
+
+        return ele;
+    }
 }
