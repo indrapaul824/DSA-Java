657c6924b1f85196fc9ab404853bcb5677a01ff1
IndraP24 <indrashis985@gmail.com>
2023-06-11 21:31:21 +0530

    Add Majority Element II better solution

--- SDE-Sheet/src/Day3_Arrays_III/Q4_MajorityElement_II.java
+++ SDE-Sheet/src/Day3_Arrays_III/Q4_MajorityElement_II.java
@@ -1,4 +1,22 @@
 package Day3_Arrays_III;
+import java.util.*;
 
 public class Q4_MajorityElement_II {
+    // Better
+    public List<Integer> majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        for (int n : nums)
+            map.put(n, map.getOrDefault(n, 0) + 1);
+
+        for (var it : map.entrySet()) {
+            if (it.getValue() > nums.length/3)
+                res.add(it.getKey());
+        }
+
+        return res;
+    }
+
+    // Optimal
+
 }
