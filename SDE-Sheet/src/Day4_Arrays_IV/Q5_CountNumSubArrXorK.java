105b40c1844ab2b5bb01ddb2dccbacdc82cf27a8
IndraP24 <indrashis985@gmail.com>
2023-06-28 23:08:24 +0530

    Add Day 4 Count Num of Sub-array with XOR k

--- SDE-Sheet/src/Day4_Arrays_IV/Q5_CountNumSubArrXorK.java
+++ SDE-Sheet/src/Day4_Arrays_IV/Q5_CountNumSubArrXorK.java
@@ -1,5 +1,17 @@
 package Day4_Arrays_IV;
+import java.util.*;
 
 public class Q5_CountNumSubArrXorK {
-    
+    public static int subarraysXor(ArrayList<Integer> arr, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int XR = 0, cnt = 0;
+        for (int n : arr) {
+            XR ^= n;
+            cnt += map.getOrDefault(XR ^ k, 0);
+            map.put(XR, map.getOrDefault(XR, 0) + 1);
+        }
+
+        return cnt;
+	}
 }
