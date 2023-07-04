7149682d26825544919b37fd4da7da449a113f82
IndraP24 <indrashis985@gmail.com>
2023-07-04 22:56:55 +0530

    Add Day 5 Delete Given Nodes LL initial commit

---
+++ SDE-Sheet/src/Day5_LL/Q6_DeleteGivenNodeLL.java
@@ -0,0 +1,13 @@
+package Day5_LL;
+
+public class Q6_DeleteGivenNodeLL {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {}
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+}
