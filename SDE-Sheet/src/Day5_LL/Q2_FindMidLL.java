71881c2923029b3669269f3b32ce7f3673598d85
IndraP24 <indrashis985@gmail.com>
2023-07-04 22:55:39 +0530

    Add Day 5 Find Mid LL solution

---
+++ SDE-Sheet/src/Day5_LL/Q2_FindMidLL.java
@@ -0,0 +1,23 @@
+package Day5_LL;
+
+public class Q2_FindMidLL {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {}
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
