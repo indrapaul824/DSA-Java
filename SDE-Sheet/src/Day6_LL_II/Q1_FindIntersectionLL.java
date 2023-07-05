81aa91f881d59444ba6c99cf642908e0a5b4f25d
IndraP24 <indrashis985@gmail.com>
2023-07-05 23:27:44 +0530

    Update Day 6 Find Intersection 1st Better solution

--- SDE-Sheet/src/Day6_LL_II/Q1_FindIntersectionLL.java
+++ SDE-Sheet/src/Day6_LL_II/Q1_FindIntersectionLL.java
@@ -1,4 +1,35 @@
 package Day6_LL_II;
+import java.util.*;
 
 public class Q1_FindIntersectionLL {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {}
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    // Better 1: Hashing
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode tempA = headA, tempB = headB;
+
+        while (tempA != null) {
+            set.add(tempA);
+            tempA = tempA.next;
+        }
+
+        while (tempB != null) {
+            if (set.contains(tempB))
+                return tempB;
+            tempB = tempB.next;
+        }
+
+        return null;
+    }
+
+    // Better 2:
 }
