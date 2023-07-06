c08000d1d4832900d32371a8fe007a2c2856e807
IndraP24 <indrashis985@gmail.com>
2023-07-06 14:34:52 +0530

    Update Day 6 Find Intersection 2nd Better and Optimal solution

--- SDE-Sheet/src/Day6_LL_II/Q1_FindIntersectionLL.java
+++ SDE-Sheet/src/Day6_LL_II/Q1_FindIntersectionLL.java
@@ -31,5 +31,58 @@ public class Q1_FindIntersectionLL {
         return null;
     }
 
-    // Better 2:
+    // Better 2: Difference in Length
+    private int getDiff(ListNode tempA, ListNode tempB) {
+        int size1 = 0, size2 = 0;
+
+        // Calculate the Length of each List
+        while (tempA != null || tempB != null) {
+            if (tempA != null) {
+                size1++;
+                tempA = tempA.next;
+            }
+            if (tempB != null) {
+                size2++;
+                tempB = tempB.next;
+            }
+        }
+
+        return size1 - size2;
+    }
+
+    public ListNode getIntersectionNode_b2(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA, temp2 = headB;
+
+        // Find the larger list and the absolute difference between them
+        int diff = getDiff(temp1, temp2);
+
+        // Shifting the larger list diff times to bring them to the same level | If diff == 0 -> no need to shift
+        if (diff > 0) {
+            // ListA is longer
+            while (diff-- != 0)  temp1 = temp1.next;
+        }
+        else if (diff < 0) {
+            // ListB is longer
+            while (diff++ != 0)  temp2 = temp2.next;
+        }
+
+        while (temp1 != temp2) {
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        return temp1;
+    }
+
+    // Optimal: Using difference but reverting to head when node==null until two are equal
+    public ListNode getIntersectionNode_op(ListNode headA, ListNode headB) {
+        ListNode temp1 = headA, temp2 = headB;
+
+        while (temp1 != temp2) {
+            temp1 = temp1 == null ? headA : temp1.next;
+            temp2 = temp2 == null ? headB : temp2.next;
+        }
+
+        return temp1;
+    }
 }
