26cb69d0713a81892b2576b44bca387db380d3fb
IndraP24 <indrashis985@gmail.com>
2023-07-06 14:37:10 +0530

    Update Day 6 Detect Cycle Optimal solution

--- SDE-Sheet/src/Day6_LL_II/Q2_DetectCycleLL.java
+++ SDE-Sheet/src/Day6_LL_II/Q2_DetectCycleLL.java
@@ -1,4 +1,26 @@
 package Day6_LL_II;
 
 public class Q2_DetectCycleLL {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {}
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow)
+                return true;
+        }
+        return false;
+    }
 }
