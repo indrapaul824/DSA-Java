dde3efc9cf9123a4a4ea20be0cc15af47b77b437
IndraP24 <indrashis985@gmail.com>
2023-07-08 21:37:45 +0530

    Add Palindrome LL initial commit

--- SDE-Sheet/src/Day6_LL_II/Q4_PalindromeLL.java
+++ SDE-Sheet/src/Day6_LL_II/Q4_PalindromeLL.java
@@ -1,4 +1,15 @@
 package Day6_LL_II;
 
 public class Q4_PalindromeLL {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {}
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+
 }
