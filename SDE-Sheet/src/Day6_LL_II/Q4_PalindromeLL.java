d4eeff5e55458a71b0f5d997911689e22c53392d
IndraP24 <indrashis985@gmail.com>
2023-07-09 22:32:01 +0530

    Update Palindrome LL optimal solution

--- SDE-Sheet/src/Day6_LL_II/Q4_PalindromeLL.java
+++ SDE-Sheet/src/Day6_LL_II/Q4_PalindromeLL.java
@@ -11,5 +11,48 @@ public class Q4_PalindromeLL {
         }
     }
 
+    private ListNode midlleNode(ListNode head) {
+        ListNode f = head, s = head;
 
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+
+        return s;
+    }
+
+    private ListNode reverseList(ListNode node) {
+        if (node == null)
+            return node;
+
+        ListNode prev = null, pres = node;
+
+        while (pres != null) {
+            ListNode next = pres.next;
+            pres.next = prev;
+            prev = pres;
+            pres = next;
+        }
+
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        if(head==null || head.next==null)
+            return true;
+
+        ListNode mid = midlleNode(head);
+        ListNode headSec = reverseList(mid);
+        ListNode rereverseHead = headSec;
+
+        // Compare both the halves
+        while (head != null && headSec != null) {
+            if (head.val != headSec.val)
+                return false;
+            head = head.next;
+            headSec = headSec.next;
+        }
+
+        return true;
+    }
 }
