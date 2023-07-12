49d5dd02212b0dd264f9673044ea53119dcc2790
IndraP24 <indrashis985@gmail.com>
2023-07-12 23:01:53 +0530

    Update LL best solutions

--- SDE-Sheet/src/Day6_LL_II/Q5_StartPointLoopLL.java
+++ SDE-Sheet/src/Day6_LL_II/Q5_StartPointLoopLL.java
@@ -1,4 +1,40 @@
 package Day6_LL_II;
 
 public class Q5_StartPointLoopLL {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {}
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        int flag = 0;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            return null;
+
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return slow;
+    }
 }
--- SDE-Sheet/src/Day6_LL_II/Q6_FlatteningLL.java
+++ SDE-Sheet/src/Day6_LL_II/Q6_FlatteningLL.java
@@ -1,4 +1,51 @@
 package Day6_LL_II;
 
 public class Q6_FlatteningLL {
+    public static class Node {
+        int data;
+        Node next;
+        Node bottom;
+
+        Node(int d)
+        {
+            data = d;
+            next = null;
+            bottom = null;
+        }
+    }
+
+    private void mergeTwoSorted(Node l1, Node l2) {
+        Node last;
+        while (l1 != null && l2 != null) {
+            last = null;
+            while (l1 != null && l1.data <= l2.data) {
+                last = l1;
+                l1 = l1.bottom;
+            }
+
+            last.bottom = l2;
+
+            Node temp = l1;
+            l1 = l2;
+            l2 = temp;
+        }
+    }
+    public Node flatten(Node root)
+    {
+        Node list1, list2;
+
+        Node res = root;
+        Node dummy = root.next;
+        root.next = null;
+
+        while (dummy != null) {
+            list1 = res;
+            list2 = dummy;
+            dummy = list2.next;
+            list2.next = null;
+            mergeTwoSorted(list1, list2);
+        }
+
+        return res;
+    }
 }
